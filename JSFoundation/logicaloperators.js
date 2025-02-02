07e2f44ef8b0563c2b25892b31cfe3605e2d947e
gururefresh <gururefresh@gmail.com>
2025-02-02 12:01:26 +0200

    logical operators file added

---
+++ JSFoundation/logicaloperators.js
@@ -0,0 +1,13 @@
+// &&   - and
+// ||   - or
+// !    - reverse
+
+isLoggedin = false;
+isPaid = false;
+
+console.log(isLoggedin && isPaid); // result will false because both var should be true or false
+
+let googleUser = true;
+let facebookUser = false;
+
+console.log(googleUser || facebookUser); // result will be true bcuz atleast 1 should be true
